4b9435f98c2c21301be2a81b61ceebfa66e21228
Vinicius Gomes <vinicius.go.oliv@gmail.com>
2021-11-04 20:20:46 -0300

    Introdução
    Texto explicativo de como jogar Devon.

---
+++ RPG Textual Grupo1TurmaA/src/Introdução.java
@@ -0,0 +1,15 @@
+public class Introdução {
+
+    public static void main(String[] args) {
+        System.out.println("\n =======================================================================================================");
+        System.out.println("\n Devon é um jogo de perguntas e respostas, aonde o jogados enfrenta desafios para progredir na historia.");
+        System.out.println("\n Os desafios são compostos por 5 questões (A,B,C,D,E), onde seram testados seus conhecimentos referente a materia de matematica .");
+        System.out.println("\n Para selecionar as respostas voce deve digitar o a letra correspodente a alternativa correta.");
+        System.out.println("\n Caso venha a errar alguma das questões, independete da etapa que esta no jogo, é game over, e o jogo recomeça.");
+    }
+
+    
+
+
+    
+}
