d1c9fcd8a916a725c531bd57fdd83aea86a2b2b7
mathias636 <64647213+mathias636@users.noreply.github.com>
2021-11-04 22:13:19 -0300

    atualizando  menu com switch

--- RPG Textual Grupo1TurmaA/src/Instrucoes.java
+++ RPG Textual Grupo1TurmaA/src/Instrucoes.java
@@ -1,4 +1,4 @@
-public class Introdução {
+public class Instrucoes {
 
     public static void main(String[] args) {
         System.out.println("\n =======================================================================================================");
@@ -7,9 +7,4 @@ public class Introdução {
         System.out.println("\n Para selecionar as respostas voce deve digitar o a letra correspodente a alternativa correta.");
         System.out.println("\n Caso venha a errar alguma das questões, independete da etapa que esta no jogo, é game over, e o jogo recomeça.");
     }
-
-    
-
-
-    
 }
--- RPG Textual Grupo1TurmaA/src/Menu.java
+++ RPG Textual Grupo1TurmaA/src/Menu.java
@@ -10,27 +10,28 @@ public class Menu {
             System.out.println("\n1- Instruções\n2- Jogar\n3- Créditos\n4- Sair");
             System.out.print("\nDigite sua opção: ");
             escolha_do_menu = scanner.nextInt();
-            // escolha_do_menu = scanner.next().charAt(0);
-            if(escolha_do_menu == 1) {
-                System.out.println("Você escolheu: Instruções");
-                // ft_instrucoes();
-            }
-            else if(escolha_do_menu == 2) {
-                System.out.println("Você escolheu: Jogar");
-                //ft_jogo();
-            }
-            else if(escolha_do_menu == 3) {
-                System.out.println("Você escolheu: Créditos");
-                //ft_créditos();
-            }
-            else if(escolha_do_menu == 4) {
-                System.out.println("Você escolheu: Sair");
-                System.exit(0);
-            }
-            else {
-                System.out.println("\nOpção inválida ou inexistente");
-                System.out.print("pressione \"Enter\" para mostrar novamente");
-                scanner.nextLine(); scanner.nextLine();
+            switch (escolha_do_menu)
+            {
+                case 1:
+                    System.out.println("Você escolheu: Instruções");
+                    // ft_instrucoes();
+                    break;
+                case 2:
+                    System.out.println("Você escolheu: Jogar");
+                    //ft_jogo();
+                    break;
+                case 3:
+                    System.out.println("Você escolheu: Créditos");
+                    //ft_créditos();
+                    break;
+                case 4:
+                    System.out.println("Você escolheu: Sair");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("\nOpção inválida ou inexistente");
+                    System.out.print("pressione \"Enter\" para mostrar novamente");
+                    scanner.nextLine(); scanner.nextLine();
             }
         }while(escolha_do_menu != 1 && escolha_do_menu != 2 && escolha_do_menu != 3 && escolha_do_menu != 4);
         scanner.close();
