93514290a24e3f4ed39a9d0f1b57438232b8595b
mathias636 <64647213+mathias636@users.noreply.github.com>
2021-11-05 23:08:26 -0300

    Alteração para UTF-8

--- RPG Textual Grupo1TurmaA/src/Instrucoes.java
+++ RPG Textual Grupo1TurmaA/src/Instrucoes.java
@@ -1,10 +1,10 @@
-public class Instrucoes {
+Ôªøpublic class Instrucoes {
 
     public static void main(String[] args) {
         System.out.println("\n =======================================================================================================");
-        System.out.println("\n Devon È um jogo de perguntas e respostas, onde o jogador enfrenta desafios para progredir na historia.");
-        System.out.println("\n Os desafios s„o compostos por 5 questıes (A,B,C,D,E), onde ser„o testados seus conhecimentos referente a matÈria de matematica.");
-        System.out.println("\n Para selecionar as respostas, vocÍ deve digitar a letra correspodente a alternativa que considera correta.");
-        System.out.println("\n N„o h· chances para erro. Caso erre qualquer quest„o, o jogo ser· reiniciado.");
+        System.out.println("\n Devon √© um jogo de perguntas e respostas, onde o jogador enfrenta desafios para progredir na historia.");
+        System.out.println("\n Os desafios s√£o compostos por 5 quest√µes (A,B,C,D,E), onde ser√£o testados seus conhecimentos referente a mat√©ria de matematica.");
+        System.out.println("\n Para selecionar as respostas, voc√™ deve digitar a letra correspodente a alternativa que considera correta.");
+        System.out.println("\n N√£o h√° chances para erro. Caso erre qualquer quest√£o, o jogo ser√° reiniciado.");
     }
 }
