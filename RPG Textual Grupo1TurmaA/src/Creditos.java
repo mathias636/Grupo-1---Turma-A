97a3fe3dccfa496f995a45d65245dbce0294c360
Vinicius Gomes <vinicius.go.oliv@gmail.com>
2021-11-08 20:14:55 -0300

    Modificação dos creditos e das intruções
    Foi organizado e formatado as informações contidas nos créditos e instruções para agrado visual.

--- RPG Textual Grupo1TurmaA/src/Creditos.java
+++ RPG Textual Grupo1TurmaA/src/Creditos.java
@@ -1,8 +1,17 @@
+package ProjetoPi;
 public class Creditos {
     public static void main(String[] args) {
-        System.out.println("\n========================================================================");
-        System.out.println("\n 'Devon' Foi desenvolvido como parte do projeto integrador do curso de Analise e desenvolvimento de Sitemas no Senac");
-        System.out.println("\n O grupo de desenvolvimento é composto por:"+"\n •Elton Freitas"+"\n •Keity Pires " + "\n •Matheus Galvão" + "\n •Milena Kamimura"+ "\n •Vinicius Gomes");
-        System.out.println("\n Obrigado a todos");
+        System.out.println("\n******************************************************************************************************************************");
+        System.out.println("** 'Devon' Foi desenvolvido como parte do projeto integrador do curso de Analise e desenvolvimento de Sitemas no Senac      **");
+        System.out.println("** O grupo de desenvolvimento é composto por:                                                                               **"+
+        "\n** • Elton Freitas                                                                                                           **"+
+        "\n** • Keity Pires                                                                                                             ** " + 
+        "\n** • Matheus Galvão                                                                                                          **" + 
+        "\n** • Milena Kamimura                                                                                                         **"+ 
+        "\n** • Vinicius Gomes                                                                                                          **" );
+        System.out.println("** Obrigado a todos                                                                                                         **");
+        System.out.println("******************************************************************************************************************************");
+
     }
+
 }
--- RPG Textual Grupo1TurmaA/src/Instrucoes.java
+++ RPG Textual Grupo1TurmaA/src/Instrucoes.java
@@ -1,10 +1,13 @@
 public class Instrucoes {
 
-    public static void main(String[] args) {
-        System.out.println("\n =======================================================================================================");
-        System.out.println("\n Devon é um jogo de perguntas e respostas, aonde o jogados enfrenta desafios para progredir na historia.");
-        System.out.println("\n Os desafios são compostos por 5 questões (A,B,C,D,E), onde seram testados seus conhecimentos referente a materia de matematica .");
-        System.out.println("\n Para selecionar as respostas voce deve digitar o a letra correspodente a alternativa correta.");
-        System.out.println("\n Caso venha a errar alguma das questões, independete da etapa que esta no jogo, é game over, e o jogo recomeça.");
+  public static void main(String[] args) {
+        System.out.println(" ============================================================================================================================================");
+        System.out.println(" =  Devon é um jogo de perguntas e respostas, aonde o jogados enfrenta desafios para progredir na historia.                                 =");
+        System.out.println(" == Os desafios são compostos por 5 questões (A,B,C,D,E), onde seram testados seus conhecimentos referente a materia de matematica .       ==");
+        System.out.println(" =  Para selecionar as respostas voce deve digitar o a letra correspodente a alternativa correta.                                           =");
+        System.out.println(" == Caso venha a errar alguma das questões, independete da etapa que esta no jogo, é game over, e o jogo recomeça.                         ==");
+        System.out.println(" ============================================================================================================================================");
+    
     }
+
 }
