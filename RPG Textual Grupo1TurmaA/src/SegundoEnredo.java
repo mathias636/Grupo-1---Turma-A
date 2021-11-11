2a667f7d4557f88023ac4a8f341772dcf842ee82
Vinicius Gomes <vinicius.go.oliv@gmail.com>
2021-11-10 22:55:06 -0300

    Adicionado efeito de digitação
    Criado uma função para efeito de digitação

--- RPG Textual Grupo1TurmaA/src/SegundoEnredo.java
+++ RPG Textual Grupo1TurmaA/src/SegundoEnredo.java
@@ -1,6 +1,8 @@
-﻿public class SegundoEnredo {
-    public static void main(String []args ) {
-        System.out.println("Alguns dias depois..." + 
+﻿import java.util.concurrent.TimeUnit;
+public class SegundoEnredo {
+    static int temp_dialog =25, temp_narrativa = 75, temp_transicao = 10; //Para rodar o jogo sem o delay nas mensagens, pode mudar o valor destas 3 variáveis para 0;
+        public static void main(String[] args) throws Exception{
+        Digita("Alguns dias depois..." + 
         "\n "+
         "\nNARRADOR: " +
         "\nDepois de uma longa jornada, Devon chegou em sua primeira batalha... \nE lá, Hasad lhe aguardava."+
@@ -11,7 +13,13 @@
         "\nSAIA DAQUI ANTES QUE VEJA MINHA FÚRIA." +        
         "\nDEVON: \nVeremos essa fúria então, Hasad."+
         "\nDEUS HASAD:"+
-       " \nVOCÊ FOI AVISADO, SEU TOLO.");
+       " \nVOCÊ FOI AVISADO, SEU TOLO.",TimeUnit.MILLISECONDS, temp_dialog);
+        }
+       public static void Digita(String mensagem, TimeUnit unit, long tempo_mensagem) throws InterruptedException {
+        for (char caractere : mensagem.toCharArray()) {
+        System.out.print(caractere);
+        unit.sleep(tempo_mensagem);
+    }
 
     }
 
