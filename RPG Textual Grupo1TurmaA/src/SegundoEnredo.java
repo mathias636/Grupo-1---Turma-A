78b60f62b0b75cb376b49025b396fdffe119d040
Vinicius Gomes <vinicius.go.oliv@gmail.com>
2021-11-10 22:28:34 -0300

    Segundo Enredo
    Criação do desenvolvimento do ato 2 da historia.

--- RPG Textual Grupo1TurmaA/src/SegundoEnredo.java
+++ RPG Textual Grupo1TurmaA/src/SegundoEnredo.java
@@ -2,8 +2,19 @@
 
 public class SegundoEnredo {
 
-    public static void main(Object args) {
-        System.out.println("SegundoEredo");
+    public static void main(String []args ) {
+        System.out.println("Alguns dias depois..." + 
+        "\n "+
+        "\nNARRADOR: " +
+        "\nDepois de uma longa jornada, Devon chegou em sua primeira batalha... \nE lá, Hasad lhe aguardava."+
+        "\n"+
+        "\nDEVON: \nVIM EMBUSCA DO SEU PODER, HASAD!. E NÃO HÁ NADA QUE ME EMPEDIRÁ DE DERROTÁ-LO. "
+        +"\nDEUS HASAD:  \nQUEM VOCÊ PENSA QUE É PARA ESTAR FALANDO COMIGO ASSIM?"+
+        "\nEU SOU O GRANDE HASAD, DEUS METAMORFO." +
+        "\nSAIA DAQUI ANTES QUE VEJA MINHA FÚRIA." +        
+        "DEVON: \nVeremos essa fúria então, Hasad."+
+        "\nDEUS HASAD:"+
+       " \nVOCÊ FOI AVISADO, SEU TOLO.");
 
     }
 
