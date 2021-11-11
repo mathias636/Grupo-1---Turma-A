9cba2a815e760add322c935e909b82e40bb993c0
Elton Freitas <91293466+EltonDev1@users.noreply.github.com>
2021-11-10 21:36:27 -0300

    Update Menu.java

--- RPG Textual Grupo1TurmaA/src/Menu.java
+++ RPG Textual Grupo1TurmaA/src/Menu.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
 public class Menu {
-    public static Scanner scanner = new  Scanner(System.in);
-    public static void main(String[] args) {
-        int escolha_do_menu;
-        System.out.println("\nNOME DO JOGO");
-        do
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static boolean main(String[] args, boolean menu_stop) {
+        int escolha_do_menu = 0;
+        System.out.print("\n-- DEVON --\n");
+        System.out.println("\n1- Instruções\n2- Jogar\n3- Créditos\n4- Sair\n");
+        System.out.print("Digite sua opção: ");
+        escolha_do_menu = scanner.nextInt();
+        switch (escolha_do_menu)
         {
             System.out.println("\n1- Instruções\n2- Jogar\n3- Créditos\n4- Sair");
             System.out.print("\nDigite sua opção: ");
@@ -34,13 +38,17 @@ public class Menu {
                 default:
                     System.out.println("\nOpção inválida ou inexistente");
                     ft_enter();
-            }
         }while(escolha_do_menu != 4);
         scanner.close();
-    }
+        
+        return menu_stop;
+        
+        }
 
-    private static void ft_enter() {
-        System.out.print("\n\nPressione \"Enter\" para mostrar o menu novamente");
-        scanner.nextLine(); scanner.nextLine();
-    }
+	private static void ft_enter() {
+		System.out.print("\n>> Pressione \"Enter\" para mostrar o MENU novamente");
+		Main.scanner.nextLine();
+		// Main.scanner.nextLine();
+
+	}
 }
